105e3854ae114c6802157545c6821d6ade8032a9
Catherine Miguel <catmig1992@gmail.com>
2023-03-02 15:56:01 -0800

    direct recursion count down function

--- solution1.py
+++ solution1.py
@@ -1,1 +1,19 @@
 # Write code for algorithm 1 below
+def count_down(from_num):
+    if from_num <= 0:
+        return
+
+    else:
+        print(from_num)
+        count_down(from_num -1)
+
+count_down(20)
+
+def count_down_loop(from_num):
+    while from_num >=0 :
+        print(from_num)
+        from_num -= 1
+
+#count_down_loop(20)
+
+
