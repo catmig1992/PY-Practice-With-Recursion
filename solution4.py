35efd076144e7fed6f02b15acaa956cab4fbbc79
Catherine Miguel <catmig1992@gmail.com>
2023-03-02 16:56:17 -0800

    wrote a function to solve exponents

--- solution4.py
+++ solution4.py
@@ -1,1 +1,12 @@
 # Write code for algorithm 4 below
+def a_to_b(a, b):
+    print(f"a: {a}, b: {b}")
+    if b < 1:
+        print("The exponent must be greater than 1")
+    elif b == 1:
+        return a
+    else:
+        return a * a_to_b(a, b-1)
+
+print(f"The power result is {a_to_b(2, 4)}")
+
