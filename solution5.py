7d8467e9411990465ded3c6d508d58682a75766f
Catherine Miguel <catmig1992@gmail.com>
2023-03-02 17:24:33 -0800

    wrote palindrome function

--- solution5.py
+++ solution5.py
@@ -1,1 +1,11 @@
 # Write code for algorithm 5 below
+def is_palindrome(input_str):
+    print(f"current input string: {input_str}")
+    if len(input_str) <= 1:
+        return True
+    else:
+        return (input_str[0] == input_str[-1] and is_palindrome(input_str[1:-1]))
+
+print("Is tacocat a palindrome")
+print(is_palindrome("tacocat"))
+
