674c090739d5d039c1c85fe480515d513ffc79a1
Catherine Miguel <catmig1992@gmail.com>
2023-03-02 16:24:04 -0800

    wrote a function to count from n to a base case number

--- solution2.py
+++ solution2.py
@@ -1,1 +1,11 @@
 # Write code for algorithm 2 below
+def natural_numbers(to_number, from_number = 1):
+    #print(f"Params for to_number {to_number}, from_number: {from_number}")
+    if from_number > to_number:
+        return
+    else:
+        print(from_number)
+        natural_numbers(to_number, from_number +1)
+
+natural_numbers(25)     
+
